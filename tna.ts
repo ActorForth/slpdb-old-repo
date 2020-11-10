7b0066669ac4f41556b965b2ff6473a6cf4fc4be
naach <napat_pat3@hotmail.com>
2020-11-10 14:07:16 +0700

    core:remove todo

--- tna.ts
+++ tna.ts
@@ -128,7 +128,6 @@ export class TNA {
                       }
                     } catch(_) { }
                     if (address && address.length > 0) {
-                      // TODO:
                         receiver.a = address;
                     }
                     xput.e = receiver;
